5517877cf62a4a05b4b878dee21d4f6fb6247158
zhao <zcy@zcyMacBookPro.local>
2019-07-02 14:45:23 +0800

    修复选择字体无效

--- ZCYReader/AppDelegate.m
+++ ZCYReader/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+#import "ViewController.h"
+
 @interface AppDelegate ()
 
 @end
@@ -16,7 +18,14 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    [self.window makeKeyAndVisible];
+    
+    ViewController *mainVC = [[ViewController alloc] init];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:mainVC];
+    self.window.rootViewController = nav;
+    
     return YES;
 }
 
--- ZCYReader/ViewController.m
+++ ZCYReader/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) UITableView *tableView;
+@property (strong, nonatomic) NSArray *arrCellData;
 
 @end
 
@@ -16,8 +19,73 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    self.arrCellData = @[@{@"name": @"每天懂一点好玩心理学", @"extensition": @"epub"},
+                         @{@"name": @"妖神记", @"extensition": @"txt"}];
+    
+    [self.view addSubview:self.tableView];
 }
 
+#pragma mark - UITableViewDatasource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.arrCellData.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    
+    NSDictionary *dicCell = self.arrCellData[indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@.%@", dicCell[@"name"], dicCell[@"extensition"]];
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSDictionary *dicCell = self.arrCellData[indexPath.row];
+    NSURL *fileURL = [[NSBundle mainBundle] URLForResource:dicCell[@"name"] withExtension:dicCell[@"extensition"]];
+    [self readBookWithFileURL:fileURL];
+}
+
+- (void)readBookWithFileURL:(NSURL *)fileURL
+{
+    if (fileURL == nil) { return; }
+    
+    // 开始阅读
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        XDSBookModel *bookModel = [XDSBookModel getLocalModelWithURL:fileURL];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            XDSReadPageViewController *readVC = [[XDSReadPageViewController alloc] init];
+            [[XDSReadManager sharedManager] setResourceURL:fileURL];
+            [[XDSReadManager sharedManager] setBookModel:bookModel];
+            [[XDSReadManager sharedManager] setRmDelegate:readVC];
+            [self presentViewController:readVC animated:YES completion:nil];
+        });
+    });
+}
+
+#pragma mark - setter
+
+- (UITableView *)tableView
+{
+    if (_tableView == nil) {
+        _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        
+        _tableView.estimatedRowHeight = 44;
+        _tableView.rowHeight = UITableViewAutomaticDimension;
+    }
+    return _tableView;
+}
 
 @end
